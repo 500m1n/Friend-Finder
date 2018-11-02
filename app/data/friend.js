2f02e1807df84f55eea62a6a7d3c1f89fbcd71b3
Danny Lee <dannyworkingraw@gmail.com>
2018-11-01 20:43:48 -0400

    pushed files

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+// FRIENDS ARRAY //
+
+var friendList = [
+    {
+      name: 'Jason Tashman',
+      favorite: '76ers',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
